df09d16558279e840f7843f7f3b4b4246103baae
Christoph Dolch <MrDolch@dolch.tech>
2025-06-19 23:21:02 +0200

    Beta-Release (#16)
    * Add poc-file
    
    * Mehr Speicher
    
    * Update POC
    
    * POC
    
    * POC
    
    * Button for create new diagrams
    
    * Fix Texts
    
    * Fix Texts
    
    * Update Readme
    
    * Update name
    
    * Replace Classes-Lists with Table
    
    * Add ProgressIndicator
    
    * Set up workflows
    
    * Fix some code smells
    
    * Fix build
    
    * Fix build
    
    * Fix build
    
    ---------
    
    Co-authored-by: chris <chris@dolch.tech>

--- src/main/kotlin/com/github/mrdolch/plantarchintellijplugin/toolWindow/DiagramFileListener.kt
+++ src/main/kotlin/com/github/mrdolch/plantarchintellijplugin/toolWindow/DiagramFileListener.kt
@@ -1,6 +1,8 @@
 package com.github.mrdolch.plantarchintellijplugin.toolWindow
 
 import com.intellij.ide.highlighter.JavaFileType
+
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.event.SelectionEvent
 import com.intellij.openapi.editor.event.SelectionListener
 import com.intellij.openapi.fileEditor.FileEditorManager
@@ -16,55 +18,59 @@ import tech.dolch.plantarch.cmd.IdeaRenderJob
 const val FILE_PREFIX_DEPENDENCY_DIAGRAM = "dependency-diagram-"
 
 class DiagramFileListener(
-    private val optionsPanel: PanelDiagramOptions
+  private val optionsPanel: PanelDiagramOptions
 ) : FileEditorManagerListener {
 
-    override fun selectionChanged(event: FileEditorManagerEvent) {
-        val newFile: VirtualFile? = event.newFile
-
-        when {
-            newFile == null -> {}
-            isDiagramFile(newFile) -> readOptionsFromDiagramFile(newFile)
-            isJavaFile(newFile) -> createOptionsFromFile(newFile, event.manager.project)
-        }
+  override fun selectionChanged(event: FileEditorManagerEvent) {
+    val newFile: VirtualFile? = event.newFile
+    when {
+      newFile == null -> {}
+      isDiagramFile(newFile) -> readOptionsFromDiagramFile(newFile)
+      isJavaFile(newFile) -> createOptionsFromFile(newFile, event.manager.project)
     }
+  }
 
-    private fun isJavaFile(file: VirtualFile): Boolean =
-        file.fileType == JavaFileType.INSTANCE
+  private fun isJavaFile(file: VirtualFile): Boolean =
+    file.fileType == JavaFileType.INSTANCE
 
-    private fun isDiagramFile(file: VirtualFile): Boolean =
-        file.name.startsWith(FILE_PREFIX_DEPENDENCY_DIAGRAM) && file.extension == "puml"
+  private fun isDiagramFile(file: VirtualFile): Boolean =
+    file.name.startsWith(FILE_PREFIX_DEPENDENCY_DIAGRAM) && file.extension == "puml"
 
-    private fun readOptionsFromDiagramFile(file: VirtualFile) {
-        // Beispiel: Lese Inhalt oder Metadaten und setze Felder im Panel
-        val content = String(file.contentsToByteArray())
-        content.lines().getOrNull(1)?.drop(1)?.let { json ->
-            val jobParams = Json.decodeFromString<IdeaRenderJob>(json)
-            optionsPanel.updatePanel(jobParams)
-            registerSelectionListener(getProjectByName(jobParams.projectName), optionsPanel)
-        }
+  private fun readOptionsFromDiagramFile(file: VirtualFile) {
+    // Beispiel: Lese Inhalt oder Metadaten und setze Felder im Panel
+    val content = String(file.contentsToByteArray())
+    content.lines().getOrNull(1)?.drop(1)?.let { json ->
+      val jobParams = Json.decodeFromString<IdeaRenderJob>(json)
+      optionsPanel.updatePanel(jobParams)
+      registerSelectionListener(getProjectByName(jobParams.projectName), optionsPanel)
     }
-    private fun createOptionsFromFile(file: VirtualFile, project: Project) {
+  }
+
+  private fun createOptionsFromFile(file: VirtualFile, project: Project) {
+    ApplicationManager.getApplication().executeOnPooledThread {
+      ApplicationManager.getApplication().runReadAction {
         val psiClassOwner = file.findPsiFile(project) as? com.intellij.psi.PsiClassOwner
         val className = psiClassOwner?.classes?.firstOrNull()?.qualifiedName
-        if(className != null) {
-            val module = ModuleUtil.findModuleForPsiElement(psiClassOwner)
-            val ideaRenderJob = createIdeaRenderJob(project, module!!, className)
-            optionsPanel.updatePanel(ideaRenderJob)
+        if (className != null) {
+          val module = ModuleUtil.findModuleForPsiElement(psiClassOwner)
+          val ideaRenderJob = createIdeaRenderJob(project, module!!, className)
+          optionsPanel.updatePanel(ideaRenderJob)
         }
+      }
     }
+  }
 
-    fun registerSelectionListener(project: Project, optionsPanel: PanelDiagramOptions) {
-        val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return
-        val selectionModel = editor.selectionModel
+  private fun registerSelectionListener(project: Project, optionsPanel: PanelDiagramOptions) {
+    val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return
+    val selectionModel = editor.selectionModel
 
-        selectionModel.addSelectionListener(object : SelectionListener {
-            override fun selectionChanged(e: SelectionEvent) {
-                val selectedText = e.newRange?.let { range ->
-                    editor.document.getText(range)
-                }
-                if (selectedText != null) optionsPanel.toggleListEntry(selectedText)
-            }
-        })
-    }
+    selectionModel.addSelectionListener(object : SelectionListener {
+      override fun selectionChanged(e: SelectionEvent) {
+        val selectedText = e.newRange?.let { range ->
+          editor.document.getText(range)
+        }
+        if (selectedText != null) optionsPanel.toggleEntryFromDiagram(selectedText)
+      }
+    })
+  }
 }
